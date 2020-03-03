0901f7f9cc64716aad69bbbafc59c2fd8cbae93c
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-03-02 17:54:24 -0700

    Add files via upload

--- FiringSolution.cs
+++ FiringSolution.cs
@@ -13,20 +13,20 @@ public class FiringSolution
         {
             return null;
         }
-        Debug.Log("Time to target: " + ttt);
+        //Debug.Log("Time to target: " + ttt);
 
         // return the firing vector
         //Vector3 delta = start - end;
         Vector3 delta = end - start;
-        Debug.Log("Vector to target: " + delta);
+        //Debug.Log("Vector to target: " + delta);
 
         Vector3 n1 = delta * 2;
         Vector3 n2 = gravity * (ttt.Value * ttt.Value);
         float d = 2 * muzzleV * ttt.Value;
         Vector3 solution = (n1 - n2) / d;
 
-        Debug.Log("solution = " + n1 + " - " + n2 + " / " + d);
-        Debug.Log("solution = " + solution);
+        //Debug.Log("solution = " + n1 + " - " + n2 + " / " + d);
+        //Debug.Log("solution = " + solution);
 
         return solution;
         //return ((delta * 2) - (gravity * (ttt.Value * ttt.Value))) / (2 * muzzleV * ttt.Value);
@@ -76,8 +76,8 @@ public class FiringSolution
         }
         else
         {
-            ttt = Mathf.Min(time0, time1);
-            //ttt = Mathf.Max(time0, time1);
+            //ttt = Mathf.Min(time0, time1);
+            ttt = Mathf.Max(time0, time1);
         }
 
         return ttt;
---
+++ PointOfImpact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfImpact
+{
+    Vector3 POI = new Vector3();
+    FiringSolution balistics = new FiringSolution();
+    float muzzleV = 10.0f;
+    GameObject player;
+    GameObject target;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        target = GameObject.Find("Target");
+    }
+    
+    public void FindPosition()
+    {
+        Nullable<Vector3> aimVector = balistics.Calculate(player.transform.position, target.transform.position, muzzleV, Physics.gravity);
+        Nullable<float> ttt = balistics.GetTimeToTarget(player.transform.position, target.transform.position, muzzleV, Physics.gravity);
+        POI.x = player.transform.position.x + (aimVector.Value.x * muzzleV * ttt.Value);
+        POI.y = 0;
+        POI.z = player.transform.position.z + (aimVector.Value.z * muzzleV * ttt.Value);
+        Debug.Log(POI);
+        target.transform.position = POI;
+        Debug.Log(target.transform.position);
+    }
+}
